74d4936ae3787e71a7e938e11bceef4d51a6a4e2
Khalid Abuhakmeh <khalid@aquabirdconsulting.com>
2016-07-20 11:24:10 -0400

    Speech Recognition Configuration Source
    Utilizing the SpeechRecognitionEngine, I am able to recognize
    configuration from audio files. The key is the name of the file
    while the content of the audio is the value.
    
    That was Sweeeeet!
    
    ![](http://media3.giphy.com/media/ry16t3EfWpD3O/giphy.gif)

---
+++ src/ConfigFromAnywhere/Configuration/Speech/SpeechConfigurationExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+
+namespace ConfigFromAnywhere.Configuration.Speech
+{
+    public static class SpeechConfigurationExtensions
+    {
+        public static IConfigurationBuilder AddSpeech(this IConfigurationBuilder builder, string speechPath, bool optional = false, bool reloadOnChange = false)
+        {
+            return AddSpeech(builder, null, speechPath, optional, reloadOnChange);
+        }
+
+        public static IConfigurationBuilder AddSpeech(this IConfigurationBuilder builder, IFileProvider provider, string speechPath,
+            bool optional, bool reloadOnChange)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+            if (string.IsNullOrEmpty(speechPath))
+                throw new ArgumentException("Argument is null or empty", nameof(speechPath));
+
+            if (provider == null)
+            {
+                if (Path.IsPathRooted(speechPath))
+                {
+                    provider = new PhysicalFileProvider(speechPath);
+                    speechPath = Path.GetFileName(speechPath);
+                }
+            }
+
+            var source = new SpeechConfigurationSource
+            {
+                FileProvider = provider,
+                Path = speechPath,
+                Optional = optional,
+                ReloadOnChange = reloadOnChange
+            };
+
+            builder.Add(source);
+            return builder;
+        }
+    }
+}
---
+++ src/ConfigFromAnywhere/Configuration/Speech/SpeechConfigurationProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace ConfigFromAnywhere.Configuration.Speech
+{
+    public class SpeechConfigurationProvider : FileConfigurationProvider
+    {
+        public SpeechConfigurationProvider(FileConfigurationSource source) : base(source)
+        {
+        }
+
+        public override void Load(Stream stream)
+        {
+            var parser = new SpeechConfigurationRecognizer();
+            Data = parser.Process(key: Path.GetFileNameWithoutExtension(Source.Path), stream: stream);
+        }
+    }
+}
